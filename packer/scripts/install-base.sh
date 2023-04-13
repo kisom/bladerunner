abaf3c76f6409bb7771903a793eee0cd0b7ab2d6
Kyle Isom <kyle@imap.cc>
2023-04-13 09:51:01 -0700

    packer: dev nodes have wifi right now.
    Later on, wpa_supplicant will be removed but it's needed now for
    testing.

--- packer/scripts/install-base.sh
+++ packer/scripts/install-base.sh
@@ -8,5 +8,5 @@ echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
 echo "==> installing base updates"
 apt-get -y update
-apt-get -y install ansible apt-transport-https ca-certificates
+apt-get -y install ansible apt-transport-https ca-certificates wpasupplicant
 apt-get -y clean
