f0abff9f6a71b7a16625d69412d05a41d725aeda
Kyle Isom <kyle@imap.cc>
2023-04-14 16:34:58 -0700

    packer: fixing networking

--- packer/scripts/setup-cdev.sh
+++ packer/scripts/setup-cdev.sh
@@ -6,6 +6,3 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "[+] installing cdev node packages"
 apt-get --force-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y install dnsmasq picocom wpasupplicant
-
-echo "[+] installing tailscale"
-curl -fsSL https://tailscale.com/install.sh | sh
--- packer/scripts/setup-ssh.sh
+++ packer/scripts/setup-ssh.sh
@@ -2,17 +2,5 @@
 
 set -euxo pipefail
 
-source /etc/os-release
-
-case "${ID}" in
-	ubuntu)
-		SYSTEM_USER=ubuntu
-		SYSTEM_HOME=/home/${SYSTEM_USER}
-		;;
-	*)
-		SYSTEM_USER=root
-		SYSTEM_HOME=/root
-		;;
-esac
 
 apt-get -y install openssh-server
